b3bac162dcf9690ba5c4f75946d3053d1ea9a536
Notika314 <notika314@gmail.com>
2019-10-08 16:47:51 -0400

    edit readme

--- mymalloc.c
+++ mymalloc.c
@@ -10,14 +10,15 @@ void *mymalloc(size_t size,char* filename,int lineNumber) {
 	//before we allocate memory, we need to check whether malloc is called for the first time
 	//for that, we use first char in myblock, myblock[0] to store some value, that will indicate that mymalloc 
 	// was called before already
-	if (myblock[0]!=(char)213) {	
-		myblock[0]=(char)213;	//mymalloc is called first time, so we need to assign value to mybloc[0]
+	if (myblock[0]!=(char)35) {	
+		myblock[0]=(char)35;	//mymalloc is called first time, so we need to assign value to mybloc[0]
 		printf("The size of entry is %d\n",(int)sizeof(entry));
 		if (size>(4096-sizeof(entry)-9)) {
 			printf("Cannot allocate that amount of space, try asking for less.\n");
 			return 0;
 		}
-		int num = 4096-size-3;
+		int num = 4096-size-3;		//num is the size of the largest available block of memory, 
+		//it is split between myblock[1] and myblock[2]
 		myblock[1] = (char)(num/100);
 		myblock[2] = (char)(num%100);
 		entry first_entry;		//create first block , with metadata
@@ -27,14 +28,15 @@ void *mymalloc(size_t size,char* filename,int lineNumber) {
 		return &myblock[5+sizeof(entry)];
 	} else {	//if we got here, than mymalloc was called before. Need to search whether we can find the block of 
 				//free memory in myblock of needed size(including metadata size, which is sizeof(entry))
-		printf("it is now %c\n",myblock[0]);
+		int num = (int)myblock[1]*100+(int)myblock[2];	//largest available memory block
+		if (num <(size+sizeof(entry))) printf("Cant allocate %d bytes, only have %d bytes available\n",(int)size,(int)(num+sizeof(entry)));
 	}
 	
 	return 0;
 }
 int main(int argc, char* argv[]) {
 	int *a;
-	a = malloc(1234);
+	a = malloc(2888);
 	int x = myblock[1]*100+myblock[2];
 	printf("there are %d bytes left after first allocation\n",x);
 	malloc(97);
