2e44497e149173f6c0abf1eeacf461e326424c9f
Notika314 <notika314@gmail.com>
2019-10-09 01:46:15 -0400

    updated malloc, need to debug

--- mymalloc.c
+++ mymalloc.c
@@ -17,7 +17,7 @@ void *mymalloc(size_t size,char* filename,int lineNumber) {
 			printf("Cannot allocate that amount of space, try asking for less.\n");
 			return 0;
 		}
-		int num = 4096-size-3;		//num is the size of the largest available block of memory, 
+		int num = 4096-size-2*sizeof(entry)-3;		//num is the size of the largest available block of memory, 
 		//it is split between myblock[1] and myblock[2]
 		myblock[1] = (char)(num/100);
 		myblock[2] = (char)(num%100);
@@ -27,18 +27,17 @@ void *mymalloc(size_t size,char* filename,int lineNumber) {
 		first_entry.next = NULL;
 		// *(struct thing*)(heap + offset) = test;
 		*(entry*)(myblock+3) = first_entry;
+
 		return &myblock[3+sizeof(entry)]; // (or &myblock[5+sizeof(entry)])???? 
 	} else {	//if we got here, than mymalloc was called before. Need to search whether we can find the block of 
 				//free memory in myblock of needed size(including metadata size, which is sizeof(entry))
 		int num = (int)myblock[1]*100+(int)myblock[2];	//largest available memory block
 		printf("need to allocate %d bytes, have %d bytes available\n",(int)size,num);
-		if (num<(size+sizeof(entry))) printf("Not enough memory. You can ask at most for %d bytes\n", (int)(num-sizeof(entry))); 
-		else {
+		// if (num<(size+sizeof(entry))) printf("Not enough memory. You can ask at most for %d bytes\n", (int)(num-sizeof(entry))); 
+		// else {
 			int ind = 3;	//this index for keeping track on what position in array myblock we currently are
 			//here we need to start searching for the block of right size
 			entry *currEntry = (entry*)(myblock+3);
-			// printf("On line 39.Entry's blocksize is %d\n",currEntry->blockSize);
-			// while ((currEntry->free=='0' || currEntry->blockSize<(size+sizeof(entry)))&&currEntry->next!=NULL) {
 			while (1) {
 				if (currEntry->blockSize>=(size+sizeof(entry)) && currEntry->free=='1') {
 					//Found appropriate block,allocate memory within this block
@@ -48,42 +47,89 @@ void *mymalloc(size_t size,char* filename,int lineNumber) {
 						newEntry->free='1';
 						newEntry->next = currEntry->next;
 						currEntry->next = newEntry;
+						num = myblock[1]*100+myblock[2];
+						num =-(size+2*sizeof(entry));
+						myblock[1] = (char)(num/100);
+						myblock[2] = (char)(num%100);
+						printf("allocated %d bytes ,line 56\n",(int)size);
 						return &myblock[3+sizeof(entry)];
 						//need to finish this part, split block into two blocks. Remember to update index
 					} else {	//block is not big enough for splitting
 						currEntry->free='0';
-						int num = myblock[1]*100+myblock[2];
+						num = myblock[1]*100+myblock[2];
+						num =-(currEntry->blockSize+2*sizeof(entry));
+						myblock[1] = (char)(num/100);
+						myblock[2] = (char)(num%100);
+						printf("allocated %d bytes ,line 65\n",(int)size);
+						if (currEntry->next==NULL) printf("next is null\n");
 						return &myblock[ind+sizeof(entry)];
 					}
 
-				} else  { //if next node is NULL- no more blocks
-					if (currEntry->next==NULL) {
-						entry newNode ;
-						newNode.blockSize = size;
-						newNode.free='0';
-						newNode.next=NULL;
-						currEntry->next = &(newNode);
-						return &myblock[ind+sizeof(entry)];
-					} else {	//block is too small, or not free, so going to the next one
-						ind = ind +(currEntry->blockSize)+sizeof(entry);
-						printf("updating index, it is now %d\n",ind);
-						currEntry = currEntry->next;
+				} else  {
+					if (currEntry->free!='1' || currEntry->blockSize<size+sizeof(entry)) {
+						ind = ind + currEntry->blockSize+sizeof(entry);
+						printf("%d,line 71\n",(ind + (int)size +(int)sizeof(entry)));
+						if (currEntry->next==NULL && (ind+size+sizeof(entry)<=4096)) { //if enough memory for the block:
+							//2 cases- split or not:
+							entry newEntry;
+							if (ind+size+sizeof(entry)*2+1<=4096) { //if can split
+								newEntry.blockSize = size;
+							} else {
+								newEntry.blockSize = 4096-ind-sizeof(entry);
+							}
+							newEntry.free='0';
+							newEntry.next = NULL;
+							currEntry->next = &newEntry;
+							return &myblock[ind+sizeof(entry)];
+						} else  if (currEntry->next ==NULL && (ind+size+sizeof(entry)>4096)){ 	//not found block with enough memory
+							printf("Cannot allocate that much memory\n");
+							return 0;
+						} else {
+							currEntry = currEntry->next;
+							printf("ind is %d\n", ind);
+							ind = ind + currEntry->blockSize+sizeof(entry);
+							printf("have to move to next node, update ind to %d\n",ind);
+						}
+						// return 0;
 					}
+
+				 //if next node is NULL- no more blocks
+					// if (currEntry->next==NULL &&(ind+size+sizeof(entry)<=4095)) {
+					// 	printf("ind is %d, line 70\n",ind);
+					// 	ind = ind + currEntry->blockSize +sizeof(entry);
+					// 	printf("ind is %d,line 72\n",ind);
+					// 	entry newNode ;
+					// 	newNode.blockSize = size;
+					// 	newNode.free='0';
+					// 	newNode.next=NULL;
+					// 	currEntry->next = &(newNode);
+					// 	printf("block is at index %d\n",(int)(ind+sizeof(entry)));
+
+					// 	//need to change num here:
+					// 	num = num - (size+sizeof(entry));
+					// 	return &myblock[ind+sizeof(entry)];
+					// }else if (currEntry->next==NULL && (ind+size+sizeof(entry)>4095)) {
+					// 	printf("Cannot find enough memory, reached the end of array\n");
+					// } else {	//block is too small, or not free, so going to the next one
+					// 	ind = ind +(currEntry->blockSize)+sizeof(entry);
+					// 	printf("updating index, it is now %d\n",ind);
+					// 	currEntry = currEntry->next;
+					// }
 				} 
 			}
-		}
+		// }
 		
 	}
 	return 0;
 }
 int main(int argc, char* argv[]) {
 	int *a;
-	a = malloc(1888);
+	a = malloc(1000);
 	int x = myblock[1]*100+myblock[2];
 	printf("there are %d bytes left after first allocation\n",x);
-	malloc(1000);
+	malloc(3065);
 	malloc(50);
-	malloc(194);
+	// malloc(194);
 	return 0;
 }
 // char mychars[10];
